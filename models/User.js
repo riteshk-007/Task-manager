37cdecf3beb4ed6c7024935bc008e58051902ca4
Ritesh <codeshorts007@gmail.com>
2023-11-06 17:59:57 +0530

    User Schema update

--- models/User.js
+++ models/User.js
@@ -1,23 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your name"],
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      trim: true,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please enter your email"],
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter your password"],
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+    timeseries: true,
+  }
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
