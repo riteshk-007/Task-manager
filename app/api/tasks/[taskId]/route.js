ab67df1605b74731e9062b97fe06a9f275c1ddaa
Ritesh <codeshorts007@gmail.com>
2023-11-07 14:16:28 +0530

    create- get one task details

--- app/api/tasks/[taskId]/route.js
+++ app/api/tasks/[taskId]/route.js
@@ -1,6 +1,19 @@
 //  api/tasks/{taskId}
 
-export const GET = async (req) => {};
-export const POST = async (req) => {};
+import Task from "@/models/Task";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  const { taskId } = params;
+  try {
+    const task = await Task.findById(taskId);
+    return NextResponse.json(task, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "failed to fetch Task!" },
+      { status: 500 }
+    );
+  }
+};
 export const PUT = async (req) => {};
 export const DELETE = async (req) => {};
