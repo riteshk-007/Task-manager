2c87f0c5b5b45038eed8a834f6726ca8c86ef416
Ritesh <codeshorts007@gmail.com>
2023-11-07 16:57:45 +0530

    style- login page

---
+++ app/login/page.js
@@ -0,0 +1,14 @@
+import LoginPage from "@/components/LoginPage";
+
+export const metadata = {
+  title: "Login Page",
+};
+const page = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default page;
---
+++ components/LoginPage.js
@@ -0,0 +1,101 @@
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <section className="text-gray-600 body-font w-full flex items-center justify-center h-screen">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <h1 className="font-bold text-white text-8xl">
+            Task<span className="text-red-600">ly</span>
+          </h1>
+          <p className="text-gray-300 text-sm font-semibold  mx-2 py-4">
+            Task management made easy. Create, assign, and track tasks with ease
+            and simplicity.
+          </p>
+        </div>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+              login to your account
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-200"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    placeholder="Email address"
+                    className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-200"
+                  >
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a
+                      href="#!"
+                      className="font-semibold text-blue-500 hover:text-blue-600"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    placeholder="********"
+                    className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Dont have an account?{" "}
+              <Link
+                href="/signup"
+                className="font-semibold leading-6 text-blue-500 hover:text-blue-600"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
