ae99dfbbd64a0762a69a426f1efd81f1c701a852
Ritesh <codeshorts007@gmail.com>
2023-11-07 14:23:29 +0530

    create- Update one Task

--- app/api/tasks/[taskId]/route.js
+++ app/api/tasks/[taskId]/route.js
@@ -9,11 +9,29 @@ export const GET = async (req, { params }) => {
     const task = await Task.findById(taskId);
     return NextResponse.json(task, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { message: "failed to fetch Task!" },
       { status: 500 }
     );
   }
 };
-export const PUT = async (req) => {};
-export const DELETE = async (req) => {};
+export const PUT = async (req, { params }) => {
+  const { taskId } = params;
+  try {
+    const { title, content, status } = await req.json();
+    let task = await Task.findById(taskId);
+    task.title = title;
+    task.content = content;
+    task.status = status;
+    task = await task.save();
+    return NextResponse.json(task, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "failed to update Task!" },
+      { status: 500 }
+    );
+  }
+};
+export const DELETE = async (req, { params }) => {};
