aa3aedbfc966c17dbc5c4a588189c166a48ad8b8
Ritesh <codeshorts007@gmail.com>
2023-11-07 16:52:51 +0530

    style- signup page

---
+++ app/signup/page.js
@@ -0,0 +1,14 @@
+import SignUpPage from "@/components/SignUpPage";
+
+export const metadata = {
+  title: "Sign Up Page",
+};
+const page = () => {
+  return (
+    <div>
+      <SignUpPage />
+    </div>
+  );
+};
+
+export default page;
