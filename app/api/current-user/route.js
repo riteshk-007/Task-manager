fb6828496affe037a4d74bc0b0de790a4b80a52b
Ritesh <codeshorts007@gmail.com>
2023-11-07 13:29:39 +0530

    create- Login User detail update API

--- app/api/current-user/route.js
+++ app/api/current-user/route.js
@@ -16,3 +16,47 @@ export const GET = async (req) => {
   }
   return NextResponse.json({ detail }, { status: 200 });
 };
+
+export const PUT = async (req) => {
+  await connectDB();
+  const authToken = cookies().get("authToken");
+  try {
+    const data = jwt.verify(authToken.value, process.env.JWT_SECRET);
+    const id = data.id;
+    let detail = await User.findById(id);
+    if (!detail) {
+      return NextResponse.json({ error: "User not found" });
+    }
+    const { name, email, password } = await req.json();
+    detail.name = name;
+    detail.email = email;
+    detail.password = password;
+    await detail.save();
+    return NextResponse.json(
+      { message: "User updated successfully" },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+};
+
+// export const DELETE = async (req) => {
+//   await connectDB();
+//   const authToken = cookies().get("authToken");
+//   try {
+//     const data = jwt.verify(authToken.value, process.env.JWT_SECRET);
+//     const id = data.id;
+//     const detail = await User.findById(id);
+//     if (!detail) {
+//       return NextResponse.json({ error: "User not found" });
+//     }
+//     await detail.remove();
+//     return NextResponse.json(
+//       { message: "User deleted successfully" },
+//       { status: 200 }
+//     );
+//   } catch (err) {
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// };
